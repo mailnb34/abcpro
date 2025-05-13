a4d9851d4049037fb591466b4cc02ef65f032b33
mailnb34 <mailnb22411ca@st.uel.edu.vn>
2025-05-14 00:50:35 +0700

    Create MainActivity.java

---
+++ app/main/java/com/mai/abcpro/MainActivity.java
@@ -0,0 +1,61 @@
+package com.mai.abcpro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgEmployee;
+    TextView txtEmployee;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        addView();
+        addEvent();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void addEvent() {
+        imgEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // gọi code mở man hình quản trị nhân sự
+                openEmployeeManagementActivity();
+            }
+        });
+        txtEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // gọi code mở màn hình
+                openEmployeeManagementActivity();
+            }
+        });
+
+    }
+
+    private void addView() {
+        imgEmployee=findViewById(R.id.imgEmployee);
+        txtEmployee=findViewById(R.id.txtEmployee);
+    }
+    private void openEmployeeManagementActivity()
+    {
+        Intent intent=new Intent(MainActivity.this, EmployeeManagementActivity.class);
+        startActivity(intent);
+    }
+}
